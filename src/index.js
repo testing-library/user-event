c00e8653d0b077cf3bb7a18888fdc02110b3bed4
Gpx <polvara@gmail.com>
2018-09-23 00:30:01 +0200

    fix: 🐛 click handles checkboxes

--- src/index.js
+++ src/index.js
@@ -1,61 +1,77 @@
-import { fireEvent } from 'dom-testing-library'
+import { fireEvent } from "dom-testing-library";
 
 function findTagInParents(element, tagName) {
-  if (element.parentNode == null) return undefined
-  if (element.parentNode.tagName === tagName) return element.parentNode
-  return findTagInParents(element.parentNode, tagName)
+  if (element.parentNode == null) return undefined;
+  if (element.parentNode.tagName === tagName) return element.parentNode;
+  return findTagInParents(element.parentNode, tagName);
 }
 
 function clickLabel(label) {
-  fireEvent.mouseOver(label)
-  fireEvent.mouseMove(label)
-  fireEvent.mouseDown(label)
-  fireEvent.mouseUp(label)
-  fireEvent.click(label)
+  fireEvent.mouseOver(label);
+  fireEvent.mouseMove(label);
+  fireEvent.mouseDown(label);
+  fireEvent.mouseUp(label);
+  fireEvent.click(label);
 
   if (label.htmlFor) {
-    const input = document.getElementById(label.htmlFor)
-    input.focus()
-    fireEvent.click(label)
+    const input = document.getElementById(label.htmlFor);
+    input.focus();
+    fireEvent.click(label);
   } else {
-    const input = label.querySelector('input,textarea')
-    input.focus()
-    label.focus()
-    fireEvent.click(input)
-    fireEvent.click(label)
+    const input = label.querySelector("input,textarea");
+    input.focus();
+    label.focus();
+    fireEvent.click(input);
+    fireEvent.click(label);
   }
 }
 
-function clickElement(element) {
-  fireEvent.mouseOver(element)
-  fireEvent.mouseMove(element)
-  fireEvent.mouseDown(element)
-  element.focus()
-  fireEvent.mouseUp(element)
-  fireEvent.click(element)
+function clickCheckbox(checkbox) {
+  fireEvent.mouseOver(checkbox);
+  fireEvent.mouseMove(checkbox);
+  fireEvent.mouseDown(checkbox);
+  fireEvent.mouseUp(checkbox);
+  fireEvent.click(checkbox);
+  fireEvent.change(checkbox);
+}
 
-  const labelAncestor = findTagInParents(element, 'LABEL')
-  labelAncestor && clickLabel(labelAncestor)
+function clickElement(element) {
+  fireEvent.mouseOver(element);
+  fireEvent.mouseMove(element);
+  fireEvent.mouseDown(element);
+  element.focus();
+  fireEvent.mouseUp(element);
+  fireEvent.click(element);
+
+  const labelAncestor = findTagInParents(element, "LABEL");
+  labelAncestor && clickLabel(labelAncestor);
 }
 
 const userEvent = {
   click(element) {
-    const focusedElement = document.activeElement
+    const focusedElement = document.activeElement;
     const wasAnotherElementFocused =
-      focusedElement !== document.body && focusedElement !== element
+      focusedElement !== document.body && focusedElement !== element;
     if (wasAnotherElementFocused) {
-      fireEvent.mouseMove(focusedElement)
-      fireEvent.mouseLeave(focusedElement)
+      fireEvent.mouseMove(focusedElement);
+      fireEvent.mouseLeave(focusedElement);
     }
 
-    if (element.tagName === 'LABEL') {
-      clickLabel(element)
-    } else {
-      clickElement(element)
+    switch (element.tagName) {
+      case "LABEL":
+        clickLabel(element);
+        break;
+      case "INPUT":
+        if (element.type === "checkbox") {
+          clickCheckbox(element);
+          break;
+        }
+      default:
+        clickElement(element);
     }
 
-    wasAnotherElementFocused && focusedElement.blur()
-  },
-}
+    wasAnotherElementFocused && focusedElement.blur();
+  }
+};
 
-export default userEvent
+export default userEvent;

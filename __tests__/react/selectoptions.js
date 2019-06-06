4ed65f37ec77d7cee02fe6f34f2af6546b30baec
Adrià Fontcuberta <afontcu@gmail.com>
2019-06-06 16:15:34 +0200

    test: 💍 add Vue tests for click event

--- __tests__/react/selectoptions.js
+++ __tests__/react/selectoptions.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import "jest-dom/extend-expect";
-import userEvent from "../src";
+import userEvent from "../../src";
 
 afterEach(cleanup);
 
--- __tests__/react/type.js
+++ __tests__/react/type.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { cleanup, render, wait } from "@testing-library/react";
 import "jest-dom/extend-expect";
-import userEvent from "../src";
+import userEvent from "../../src";
 
 afterEach(cleanup);
 

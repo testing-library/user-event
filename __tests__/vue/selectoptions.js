621b709d3c07d9afa053083abd51c00be21963e7
Adrià Fontcuberta <afontcu@gmail.com>
2019-07-21 19:32:04 +0200

    Add passing selectoptions tests

---
+++ __tests__/vue/selectoptions.js
@@ -0,0 +1,207 @@
+import { render, cleanup } from "@testing-library/vue";
+import "jest-dom/extend-expect";
+import userEvent from "../../src";
+
+afterEach(cleanup);
+
+describe("userEvent.selectOptions", () => {
+  it.each(["select", "select multiple"])(
+    "should fire the correct events for <%s>",
+    type => {
+      const events = [];
+      const eventsHandler = jest.fn(evt => events.push(evt.type));
+      const multiple = type === "select multiple";
+      const eventHandlers = {
+        mouseover: eventsHandler,
+        mousemove: eventsHandler,
+        mousedown: eventsHandler,
+        focus: eventsHandler,
+        mouseup: eventsHandler,
+        click: eventsHandler
+      };
+
+      const { getByTestId } = render({
+        render: function(h) {
+          return h(
+            "select",
+            {
+              attrs: {
+                "data-testid": "element",
+                ...(multiple && { multiple: true })
+              },
+              on: eventHandlers
+            },
+            [
+              h("option", { attrs: { value: "1" } }, "1"),
+              h("option", { attrs: { value: "2" } }, "2"),
+              h("option", { attrs: { value: "3" } }, "3")
+            ]
+          );
+        }
+      });
+
+      userEvent.selectOptions(getByTestId("element"), "1");
+
+      expect(events).toEqual([
+        "mouseover",
+        "mousemove",
+        "mousedown",
+        "focus",
+        "mouseup",
+        "click",
+        "mouseover", // The events repeat because we click on the child OPTION too
+        "mousemove", // But these specifically are the events bubbling up to the <select>
+        "mousedown",
+        // "focus", // Focus event isn't being emitted?
+        "mouseup",
+        "click"
+      ]);
+    }
+  );
+
+  it("should fire the correct events on selected OPTION child with <select>", () => {
+    function handleEvent(evt) {
+      const optValue = parseInt(evt.target.value);
+      events[optValue] = [...(events[optValue] || []), evt.type];
+    }
+
+    const events = [];
+    const eventsHandler = jest.fn(handleEvent);
+    const eventHandlers = {
+      mouseover: eventsHandler,
+      mousemove: eventsHandler,
+      mousedown: eventsHandler,
+      focus: eventsHandler,
+      mouseup: eventsHandler,
+      click: eventsHandler
+    };
+
+    const { getByTestId } = render({
+      render: function(h) {
+        return h(
+          "select",
+          {
+            attrs: {
+              "data-testid": "element"
+            }
+          },
+          [
+            h("option", { attrs: { value: "1" }, on: eventHandlers }, "1"),
+            h("option", { attrs: { value: "2" }, on: eventHandlers }, "2"),
+            h("option", { attrs: { value: "3" }, on: eventHandlers }, "3")
+          ]
+        );
+      }
+    });
+
+    userEvent.selectOptions(getByTestId("element"), ["2"]);
+
+    expect(events[1]).toBe(undefined);
+    expect(events[3]).toBe(undefined);
+    expect(events[2]).toEqual([
+      "mouseover",
+      "mousemove",
+      "mousedown",
+      "focus",
+      "mouseup",
+      "click"
+    ]);
+  });
+
+  it("should fire the correct events on selected OPTION children with <select multiple>", () => {
+    function handleEvent(evt) {
+      const optValue = parseInt(evt.target.value);
+      events[optValue] = [...(events[optValue] || []), evt.type];
+    }
+
+    const events = [];
+    const eventsHandler = jest.fn(handleEvent);
+    const eventHandlers = {
+      mouseover: eventsHandler,
+      mousemove: eventsHandler,
+      mousedown: eventsHandler,
+      focus: eventsHandler,
+      mouseup: eventsHandler,
+      click: eventsHandler
+    };
+
+    const { getByTestId } = render({
+      render: function(h) {
+        return h(
+          "select",
+          {
+            attrs: {
+              "data-testid": "element",
+              multiple: true
+            }
+          },
+          [
+            h("option", { attrs: { value: "1" }, on: eventHandlers }, "1"),
+            h("option", { attrs: { value: "2" }, on: eventHandlers }, "2"),
+            h("option", { attrs: { value: "3" }, on: eventHandlers }, "3")
+          ]
+        );
+      }
+    });
+
+    userEvent.selectOptions(getByTestId("element"), ["1", "3"]);
+
+    expect(events[2]).toBe(undefined);
+    expect(events[1]).toEqual([
+      "mouseover",
+      "mousemove",
+      "mousedown",
+      "focus",
+      "mouseup",
+      "click"
+    ]);
+
+    expect(events[3]).toEqual([
+      "mouseover",
+      "mousemove",
+      "mousedown",
+      "focus",
+      "mouseup",
+      "click"
+    ]);
+  });
+
+  it("sets the selected prop on the selected OPTION", () => {
+    const onSubmit = jest.fn();
+
+    const { getByTestId } = render({
+      render: function(h) {
+        return h("form", { on: { submit: onSubmit } }, [
+          h(
+            "select",
+            {
+              attrs: {
+                "data-testid": "element",
+                multiple: true
+              }
+            },
+            [
+              h(
+                "option",
+                { attrs: { value: "1", "data-testid": "val1" } },
+                "1"
+              ),
+              h(
+                "option",
+                { attrs: { value: "2", "data-testid": "val2" } },
+                "2"
+              ),
+              h("option", { attrs: { value: "3", "data-testid": "val3" } }, "3")
+            ]
+          )
+        ]);
+      }
+    });
+
+    userEvent.selectOptions(getByTestId("element"), ["1", "3"]);
+
+    expect(getByTestId("val1").selected).toBe(true);
+    expect(getByTestId("val2").selected).toBe(false);
+    expect(getByTestId("val3").selected).toBe(true);
+  });
+});

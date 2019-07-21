1caa42b868561371d0d332fe8478cfc01e4d5bda
Adrià Fontcuberta <afontcu@gmail.com>
2019-07-21 19:31:32 +0200

    Remove unused import

--- __tests__/react/selectoptions.js
+++ __tests__/react/selectoptions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "../../src";
 

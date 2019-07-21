b12f5a5e184013aa61ba15fe29240bea62faa38b
Adrià Fontcuberta <afontcu@gmail.com>
2019-07-21 19:31:45 +0200

    Add passing dblclick tests

---
+++ __tests__/vue/dblclick.js
@@ -0,0 +1,127 @@
+import { render, cleanup } from "@testing-library/vue";
+import "jest-dom/extend-expect";
+import userEvent from "../../src";
+
+afterEach(cleanup);
+
+describe("userEvent.dblClick", () => {
+  it.each(["input", "textarea"])(
+    "should fire the correct events for <%s>",
+    type => {
+      const events = [];
+      const eventsHandler = jest.fn(evt => events.push(evt.type));
+      const { getByTestId } = render({
+        render: function(h) {
+          return h(type, {
+            attrs: {
+              "data-testid": "element"
+            },
+            on: {
+              mouseover: eventsHandler,
+              mousemove: eventsHandler,
+              mousedown: eventsHandler,
+              focus: eventsHandler,
+              mouseup: eventsHandler,
+              click: eventsHandler,
+              dblclick: eventsHandler
+            }
+          });
+        }
+      });
+
+      userEvent.dblClick(getByTestId("element"));
+
+      expect(events).toEqual([
+        "mouseover",
+        "mousemove",
+        "mousedown",
+        "focus",
+        "mouseup",
+        "click",
+        "mousedown",
+        "mouseup",
+        "click",
+        "dblclick"
+      ]);
+    }
+  );
+
+  it('should fire the correct events for <input type="checkbox">', () => {
+    const events = [];
+    const eventsHandler = jest.fn(evt => events.push(evt.type));
+
+    const { getByTestId } = render({
+      render: function(h) {
+        return h("input", {
+          attrs: {
+            type: "checkbox",
+            "data-testid": "element"
+          },
+          on: {
+            mouseover: eventsHandler,
+            mousemove: eventsHandler,
+            mousedown: eventsHandler,
+            focus: eventsHandler,
+            mouseup: eventsHandler,
+            click: eventsHandler,
+            change: eventsHandler
+          }
+        });
+      }
+    });
+
+    userEvent.dblClick(getByTestId("element"));
+
+    expect(events).toEqual([
+      "mouseover",
+      "mousemove",
+      "mousedown",
+      "mouseup",
+      "click",
+      "change",
+      "change", // Duplicate change event
+      "mousedown",
+      "mouseup",
+      "click",
+      "change",
+      "change" // Duplicate change event
+    ]);
+
+    expect(getByTestId("element")).toHaveProperty("checked", false);
+  });
+
+  it("should fire the correct events for <div>", () => {
+    const events = [];
+    const eventsHandler = jest.fn(evt => events.push(evt.type));
+    const { getByTestId } = render({
+      render: function(h) {
+        return h("div", {
+          attrs: {
+            "data-testid": "div"
+          },
+          on: {
+            mouseover: eventsHandler,
+            mousemove: eventsHandler,
+            mousedown: eventsHandler,
+            focus: eventsHandler,
+            mouseup: eventsHandler,
+            click: eventsHandler,
+            change: eventsHandler
+          }
+        });
+      }
+    });
+
+    userEvent.dblClick(getByTestId("div"));
+    expect(events).toEqual([
+      "mouseover",
+      "mousemove",
+      "mousedown",
+      "mouseup",
+      "click",
+      "mousedown",
+      "mouseup",
+      "click"
+    ]);
+  });
+});

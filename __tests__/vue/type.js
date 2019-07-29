5cab6b1a111d205b07f0227d7159e72383dd3b56
Adrià Fontcuberta <afontcu@gmail.com>
2019-07-29 20:36:24 +0200

    Keep type tests up to date

--- __tests__/vue/type.js
+++ __tests__/vue/type.js
@@ -1,4 +1,4 @@
-import { cleanup, render, wait } from "@testing-library/vue";
+import { cleanup, render, wait, fireEvent } from "@testing-library/vue";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "../../src";
 
@@ -16,68 +16,78 @@ const renderComponent = (type, events = {}) =>
 
 describe("userEvent.type", () => {
   it.each(["input", "textarea"])("should type text in <%s>", type => {
-    const change = jest.fn();
+    const input = jest.fn();
 
-    const { getByTestId } = renderComponent(type, { change });
+    const { getByTestId } = renderComponent(type, { input });
 
     const text = "Hello, world!";
     userEvent.type(getByTestId("input"), text);
 
-    expect(change).toHaveBeenCalledTimes(text.length);
+    expect(input).toHaveBeenCalledTimes(text.length);
     expect(getByTestId("input")).toHaveProperty("value", text);
   });
 
   it("should not type when event.preventDefault() is called", () => {
-    const change = jest.fn();
+    const input = jest.fn();
     const keydown = jest
       .fn()
       .mockImplementation(event => event.preventDefault());
 
-    const { getByTestId } = renderComponent("input", { change, keydown });
+    const { getByTestId } = renderComponent("input", { input, keydown });
 
     const text = "Hello, world!";
     userEvent.type(getByTestId("input"), text);
     expect(keydown).toHaveBeenCalledTimes(text.length);
-    // expect(change).toHaveBeenCalledTimes(0);
+    // expect(input).toHaveBeenCalledTimes(0);
     // expect(getByTestId("input")).not.toHaveProperty("value", text);
   });
 
-  it("should delayed the typing when opts.dealy is not 0", async () => {
+  it("should delay the typing when opts.delay is not 0", async () => {
     jest.useFakeTimers();
-    const onChange = jest.fn();
-
-    const { getByTestId } = renderComponent("input", { change: onChange });
+    const change = jest.fn();
+    const input = jest.fn();
+    const { getByTestId } = renderComponent("input", { change, input });
     const text = "Hello, world!";
     const delay = 10;
+
     userEvent.type(getByTestId("input"), text, {
       delay
     });
-    expect(onChange).not.toHaveBeenCalled();
+
+    expect(input).not.toHaveBeenCalled();
     expect(getByTestId("input")).not.toHaveProperty("value", text);
 
     for (let i = 0; i < text.length; i++) {
       jest.advanceTimersByTime(delay);
-      await wait(() => expect(onChange).toHaveBeenCalledTimes(i + 1));
+
+      await wait(() => expect(input).toHaveBeenCalledTimes(i + 1));
+
       expect(getByTestId("input")).toHaveProperty(
         "value",
         text.slice(0, i + 1)
       );
     }
+
+    // Vue's change event is emitted until blurring the input
+    expect(change).not.toHaveBeenCalled();
+    fireEvent.blur(getByTestId("input"));
+    await wait(() => expect(change).toHaveBeenCalledTimes(1));
   });
 
   it.each(["input", "textarea"])(
     "should type text in <%s> all at once",
     type => {
-      const change = jest.fn();
+      const input = jest.fn();
 
-      const { getByTestId } = renderComponent(type, { change });
+      const { getByTestId } = renderComponent(type, { input });
       const text = "Hello, world!";
+
       userEvent.type(getByTestId("input"), text, {
         allAtOnce: true
       });
 
-      expect(change).toHaveBeenCalledTimes(1);
       expect(getByTestId("input")).toHaveProperty("value", text);
+      expect(input).toHaveBeenCalledTimes(1);
     }
   );
 });

0c971f85c6a80631ab16bd90e7e72d3906181202
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-04-27 15:20:42 +0800

    fix: type definition for toggleOptions

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,26 +1,27 @@
 // Definitions by: Wu Haotian <https://github.com/whtsky>
 export interface IUserOptions {
-    allAtOnce?: boolean;
-    delay?: number;
+  allAtOnce?: boolean;
+  delay?: number;
 }
 
 export interface ITabUserOptions {
-    shift?: boolean;
-    focusTrap?: Document | Element;
+  shift?: boolean;
+  focusTrap?: Document | Element;
 }
 
 export type TargetElement = Element | Window;
 
 declare const userEvent: {
-    click: (element: TargetElement) => void;
-    dblClick: (element: TargetElement) => void;
-    selectOptions: (element: TargetElement, values: string | string[]) => void;
-    type: (
-        element: TargetElement,
-        text: string,
-        userOpts?: IUserOptions
-    ) => Promise<void>;
-    tab: (userOpts?: ITabUserOptions) => void;
+  click: (element: TargetElement) => void;
+  dblClick: (element: TargetElement) => void;
+  selectOptions: (element: TargetElement, values: string | string[]) => void;
+  toggleOptions: (element: TargetElement, values: string | string[]) => void;
+  type: (
+    element: TargetElement,
+    text: string,
+    userOpts?: IUserOptions
+  ) => Promise<void>;
+  tab: (userOpts?: ITabUserOptions) => void;
 };
 
 export default userEvent;

3d1fd2596d05821405facdd0a518c37daa34547a
Joerg Bayreuther <jb@visualjerk.de>
2020-07-22 20:02:51 +0200

    fix: remove duplicate focusIn and focusOut

--- src/blur.js
+++ src/blur.js
@@ -1,14 +1,12 @@
-import {fireEvent} from '@testing-library/dom'
 import {getActiveElement, isFocusable, eventWrapper} from './utils'
 
-function blur(element, init) {
+function blur(element) {
   if (!isFocusable(element)) return
 
   const wasActive = getActiveElement(element.ownerDocument) === element
   if (!wasActive) return
 
   eventWrapper(() => element.blur())
-  fireEvent.focusOut(element, init)
 }
 
 export {blur}

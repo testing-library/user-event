6fa71631f8273c59023b59ba3befd204b8cd47d9
Marco Moretti <marcomoretti0103@gmail.com>
2020-08-08 01:58:03 +0200

    fix: improve submit form on ENTER pressed  (#428)
    * test: add failing tests
    
    * fix: submit form on enter pressing
    
    When enter button is pressed on an input it should trigger a form submission when:
    1) A form is present
    2) The form contains only one input or when there is a submit button (input[type="submit" or button[type="submit"])
    
    * test: split test into multiple ones

--- src/__tests__/type.js
+++ src/__tests__/type.js
@@ -853,8 +853,48 @@ test('typing on input type file should not result in an error', () => {
   return userEvent.type(element, 'bar').catch(e => expect(e).toBeUndefined())
 })
 
-test('should submit a form when ENTER is pressed on input', () => {
+test('should submit a form containing multiple text inputs and an input of type submit', () => {
   const handleSubmit = jest.fn()
+  const {element} = setup(
+    `
+    <form>
+      <input type='text'/>
+      <input type='text'/>
+      <input type='submit' value="submit" />
+    </form>
+  `,
+    {
+      eventHandlers: {submit: handleSubmit},
+    },
+  )
+  userEvent.type(element.querySelector('input'), '{enter}')
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('should submit a form containing multiple text inputs and a submit button', () => {
+  const handleSubmit = jest.fn()
+
+  const {element} = setup(
+    `
+    <form>
+      <input type='text'/>
+      <input type='text'/>
+      <button type='submit' value="submit" />
+    </form>
+  `,
+    {
+      eventHandlers: {submit: handleSubmit},
+    },
+  )
+  userEvent.type(element.querySelector('input'), '{enter}')
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('should submit a form with one input element', () => {
+  const handleSubmit = jest.fn()
+
   const {element} = setup(
     `
     <form>
@@ -868,6 +908,24 @@ test('should submit a form when ENTER is pressed on input', () => {
   userEvent.type(element.querySelector('input'), '{enter}')
 
   expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('should not submit a form with multiple input when ENTER is pressed on one of it', () => {
+  const handleSubmit = jest.fn()
+  const {element} = setup(
+    `
+    <form>
+      <input type='text'/>
+      <input type='text'/>
+    </form>
+  `,
+    {
+      eventHandlers: {submit: handleSubmit},
+    },
+  )
+  userEvent.type(element.querySelector('input'), '{enter}')
+
+  expect(handleSubmit).toHaveBeenCalledTimes(0)
 })
 
 test('should type inside a contenteditable div', () => {

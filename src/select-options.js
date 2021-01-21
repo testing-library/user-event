6a02a14906b186b1144767247c57c9801d50fd13
Philipp Fritsche <ph.fritsche@gmail.com>
2021-01-21 15:19:44 +0100

    fix: selectOptions (#543)
    * fix: selectOptions on options with role #542
    
    * fix: selectOptions click event on option
    
    * fix: aria-selected
    
    * fix: handle invalid element

--- src/select-options.js
+++ src/select-options.js
@@ -36,53 +36,70 @@ function selectOptionsBase(newValue, select, values, init) {
 
   if (select.disabled || !selectedOptions.length) return
 
-  if (select.multiple) {
-    for (const option of selectedOptions) {
-      // events fired for multiple select are weird. Can't use hover...
-      fireEvent.pointerOver(option, init)
+  if (select instanceof HTMLSelectElement) {
+    if (select.multiple) {
+      for (const option of selectedOptions) {
+        // events fired for multiple select are weird. Can't use hover...
+        fireEvent.pointerOver(option, init)
+        fireEvent.pointerEnter(select, init)
+        fireEvent.mouseOver(option)
+        fireEvent.mouseEnter(select)
+        fireEvent.pointerMove(option, init)
+        fireEvent.mouseMove(option, init)
+        fireEvent.pointerDown(option, init)
+        fireEvent.mouseDown(option, init)
+        focus(select, init)
+        fireEvent.pointerUp(option, init)
+        fireEvent.mouseUp(option, init)
+        selectOption(option)
+        fireEvent.click(option, init)
+      }
+    } else if (selectedOptions.length === 1) {
+      // the click to open the select options
+      click(select, init)
+
+      selectOption(selectedOptions[0])
+
+      // the browser triggers another click event on the select for the click on the option
+      // this second click has no 'down' phase
+      fireEvent.pointerOver(select, init)
       fireEvent.pointerEnter(select, init)
-      fireEvent.mouseOver(option)
+      fireEvent.mouseOver(select)
       fireEvent.mouseEnter(select)
-      fireEvent.pointerMove(option, init)
-      fireEvent.mouseMove(option, init)
-      fireEvent.pointerDown(option, init)
-      fireEvent.mouseDown(option, init)
-      focus(select, init)
-      fireEvent.pointerUp(option, init)
-      fireEvent.mouseUp(option, init)
-      selectOption(option)
-      fireEvent.click(option, init)
+      fireEvent.pointerUp(select, init)
+      fireEvent.mouseUp(select, init)
+      fireEvent.click(select, init)
+    } else {
+      throw getConfig().getElementError(
+        `Cannot select multiple options on a non-multiple select`,
+        select,
+      )
     }
-  } else if (selectedOptions.length === 1) {
-    click(select, init)
-    selectOption(selectedOptions[0])
+  } else if (select.getAttribute('role') === 'listbox') {
+    selectedOptions.forEach(option => {
+      hover(option, init)
+      click(option, init)
+      unhover(option, init)
+    })
   } else {
     throw getConfig().getElementError(
-      `Cannot select multiple options on a non-multiple select`,
+      `Cannot select options on elements that are neither select nor listbox elements`,
       select,
     )
   }
 
   function selectOption(option) {
-    if (option.getAttribute('role') === 'option') {
-      option?.setAttribute?.('aria-selected', newValue)
-
-      hover(option, init)
-      click(option, init)
-      unhover(option, init)
-    } else {
-      option.selected = newValue
-      fireEvent(
-        select,
-        createEvent('input', select, {
-          bubbles: true,
-          cancelable: false,
-          composed: true,
-          ...init,
-        }),
-      )
-      fireEvent.change(select, init)
-    }
+    option.selected = newValue
+    fireEvent(
+      select,
+      createEvent('input', select, {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+        ...init,
+      }),
+    )
+    fireEvent.change(select, init)
   }
 }
 

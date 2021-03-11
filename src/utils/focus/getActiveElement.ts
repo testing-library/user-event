3944223ae87622a37e51289a92a138ca5b8f296d
Philipp Fritsche <ph.fritsche@gmail.com>
2021-03-11 14:31:12 +0100

    fix: activeElement is never disabled

--- src/utils/focus/getActiveElement.ts
+++ src/utils/focus/getActiveElement.ts
@@ -1,10 +1,22 @@
+import { isDisabled } from "../misc"
+
 export function getActiveElement(
   document: Document | ShadowRoot,
 ): Element | null {
   const activeElement = document.activeElement
+
   if (activeElement?.shadowRoot) {
     return getActiveElement(activeElement.shadowRoot)
   } else {
+
+    // Browser does not yield disabled elements as document.activeElement - jsdom does
+    if (isDisabled(activeElement)) {
+      return document.ownerDocument
+        // TODO: verify behavior in ShadowRoot
+        ? document.ownerDocument.body
+        : document.body
+    }
+
     return activeElement
   }
 }

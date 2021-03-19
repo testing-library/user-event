c8c1e79c43522d6ae53a4e1f1beafe22361b8690
Philipp Fritsche <ph.fritsche@gmail.com>
2021-03-19 16:00:25 +0100

    fix: remove declaration files

--- src/paste.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function paste(
-  element: Element,
-  text: string,
-  init?: MouseEventInit,
-  pasteOptions?: {
-    initialSelectionStart?: number
-    initialSelectionEnd?: number
-  },
-): void

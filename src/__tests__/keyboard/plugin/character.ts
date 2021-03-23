05a49d5728a234ca9d139225869cf209217e3316
Philipp Fritsche <ph.fritsche@gmail.com>
2021-03-23 14:35:52 +0100

    test: bump coverage

--- src/__tests__/keyboard/plugin/character.ts
+++ src/__tests__/keyboard/plugin/character.ts
@@ -4,19 +4,21 @@ import {setup} from '__tests__/helpers/utils'
 test('type [Enter] in textarea', () => {
   const {element, getEvents} = setup(`<textarea>f</textarea>`)
 
-  userEvent.type(element as HTMLTextAreaElement, 'oo[Enter]bar')
-
-  expect(element).toHaveValue('foo\nbar')
-  expect(getEvents('input')[2]).toHaveProperty('inputType', 'insertLineBreak')
-})
-
-test('type [Enter] in contenteditable', () => {
-  const {element, getEvents} = setup(`<div contenteditable="true">f</div>`)
-
   userEvent.type(
     element as HTMLTextAreaElement,
     'oo[Enter]bar[ShiftLeft>][Enter]baz',
   )
+
+  expect(element).toHaveValue('foo\nbar\nbaz')
+  expect(getEvents('input')[2]).toHaveProperty('inputType', 'insertLineBreak')
+  expect(getEvents('input')[6]).toHaveProperty('inputType', 'insertLineBreak')
+})
+
+test('type [Enter] in contenteditable', () => {
+  const {element, getEvents} = setup(`<div contenteditable="true">f</div>`)
+  ;(element as HTMLDivElement).focus()
+
+  userEvent.keyboard('oo[Enter]bar[ShiftLeft>][Enter]baz')
 
   expect(element).toHaveTextContent('foo bar baz')
   expect(element?.firstChild).toHaveProperty('nodeValue', 'foo\nbar\nbaz')

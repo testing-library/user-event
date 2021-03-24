dff124d3386a743c7063736102816062dbd3c2b0
Philipp Fritsche <ph.fritsche@gmail.com>
2021-03-24 14:48:13 +0100

    wip: selectionRange

--- src/utils/edit/setSelectionRangeIfNecessary.ts
+++ src/utils/edit/setSelectionRangeIfNecessary.ts
@@ -1,5 +1,15 @@
-import {isContentEditable} from './isContentEditable'
+import { isElementType } from '../misc/isElementType'
 import {getSelectionRange} from './getSelectionRange'
+import { isContentEditable } from './isContentEditable'
+
+// https://github.com/jsdom/jsdom/blob/c2fb8ff94917a4d45e2398543f5dd2a8fed0bdab/lib/jsdom/living/nodes/HTMLInputElement-impl.js#L45
+enum selectionAllowedType {
+  'text' = 'text',
+  'search' = 'search',
+  'url' = 'url',
+  'tel' = 'tel',
+  'password' = 'password',
+}
 
 export function setSelectionRangeIfNecessary(
   element: Element,
@@ -16,12 +26,30 @@ export function setSelectionRangeIfNecessary(
     // cannot set selection
     return
   }
-  if (
-    selectionStart !== newSelectionStart ||
-    selectionEnd !== newSelectionStart
-  ) {
-    if (isContentEditable(element)) {
-      const range = element.ownerDocument.createRange()
+
+  if (selectionStart === newSelectionStart && selectionEnd === newSelectionEnd) {
+    return
+  }
+
+  if (isElementType(element, 'textarea')) {
+    element.setSelectionRange(newSelectionStart, newSelectionEnd)
+
+  } else if (isElementType(element, 'input')) {
+    const elementType = element.type
+    const setSelectionWorkaround = Boolean(selectionAllowedType[elementType as keyof typeof selectionAllowedType])
+
+    if (setSelectionWorkaround) {
+      element.type = 'text'
+    }
+
+    element.setSelectionRange(newSelectionStart, newSelectionEnd)
+
+    if (setSelectionWorkaround) {
+      element.type = elementType
+    }
+
+  } else {
+    const range = element.ownerDocument.createRange()
       range.selectNodeContents(element)
 
       // istanbul ignore else
@@ -36,11 +64,5 @@ export function setSelectionRangeIfNecessary(
         selection.removeAllRanges()
         selection.addRange(range)
       }
-    } else {
-      ;(element as HTMLInputElement).setSelectionRange(
-        newSelectionStart,
-        newSelectionEnd,
-      )
-    }
   }
 }

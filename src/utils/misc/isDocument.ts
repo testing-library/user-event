f3c89d0f29f352f8266bade6de001183e72a39b4
Eugene Ghanizadeh <ghanizadeh.eugene@gmail.com>
2021-04-06 19:08:44 +0200

    Update src/utils/misc/isDocument.ts
    Co-authored-by: Philipp Fritsche <ph.fritsche@gmail.com>

--- src/utils/misc/isDocument.ts
+++ src/utils/misc/isDocument.ts
@@ -1,3 +1,3 @@
-export function isDocument(el?: Document | Element): el is Document {
-  return !!el && el.nodeType === el.DOCUMENT_NODE
+export function isDocument(el: Document | Element): el is Document {
+  return el.nodeType === el.DOCUMENT_NODE
 }

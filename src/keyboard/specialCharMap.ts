9519979199e6724395ba1a9776e243e8b74bffb6
Philipp Fritsche <ph.fritsche@gmail.com>
2021-10-15 12:40:50 +0200

    fix(keyboard): deprecate `specialCharMap` (#738)

--- src/keyboard/specialCharMap.ts
+++ src/keyboard/specialCharMap.ts
@@ -1,3 +1,8 @@
+/**
+ * @deprecated This list of strings with special meaning is no longer necessary
+ * as we've introduced a standardized way to describe any keystroke for `userEvent`.
+ * @see https://testing-library.com/docs/ecosystem-user-event#keyboardtext-options
+ */
 export const specialCharMap = {
   arrowLeft: '{arrowleft}',
   arrowRight: '{arrowright}',

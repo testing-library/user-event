e69201cc18ee2e7cbd566a7a1b160bd244715606
Philipp Fritsche <ph.fritsche@gmail.com>
2021-10-19 20:45:33 +0200

    feat: add `setup` API (#746)
    * feat: add `setup` API
    
    * test: use mocking to test wiring of setup
    
    * test: prevent unhandled rejection for invalid arg type
    
    * refactor: define exported apis in setup

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-import {click, dblClick} from './click'
-import {type} from './type'
-import {clear} from './clear'
-import {tab} from './tab'
-import {hover, unhover} from './hover'
-import {upload} from './upload'
-import {selectOptions, deselectOptions} from './select-options'
-import {paste} from './paste'
-import {keyboard, specialCharMap} from './keyboard'
+import {specialCharMap} from './keyboard'
+import {userEventApis, UserEventApis, setup} from './setup'
 
-const userEvent = {
-  click,
-  dblClick,
-  type,
-  clear,
-  tab,
-  hover,
-  unhover,
-  upload,
-  selectOptions,
-  deselectOptions,
-  paste,
-  keyboard,
+const userEvent: UserEventApis & {
+  setup: typeof setup
+} = {
+  ...userEventApis,
+  setup,
 }
 
 export default userEvent

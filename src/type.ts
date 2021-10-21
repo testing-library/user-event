81d7c033ab4209c502c42b2c406891cb86addf27
Philipp Fritsche <ph.fritsche@gmail.com>
2021-10-21 08:56:15 +0000

    refactor: merge files for `type`

--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,13 @@
+import {getConfig as getDOMTestingLibraryConfig} from '@testing-library/dom'
+import {prepareDocument} from './document'
+import type {UserEvent} from './setup'
 import {
   setSelectionRange,
   getSelectionRange,
   getValue,
   getActiveElement,
-} from '../utils'
-import {keyboardImplementationWrapper} from '../keyboard'
-import {UserEvent} from '../setup'
+} from './utils'
+import {keyboardImplementationWrapper} from './keyboard'
 
 export interface typeOptions {
   delay?: number
@@ -15,8 +17,45 @@ export interface typeOptions {
   initialSelectionEnd?: number
 }
 
-export async function typeImplementation(
+export function type(
   this: UserEvent,
+  element: Element,
+  text: string,
+  options?: typeOptions & {delay?: 0},
+): void
+export function type(
+  this: UserEvent,
+  element: Element,
+  text: string,
+  options: typeOptions & {delay: number},
+): Promise<void>
+// this needs to be wrapped in the event/asyncWrapper for React's act and angular's change detection
+// depending on whether it will be async.
+export function type(
+  this: UserEvent,
+  element: Element,
+  text: string,
+  {delay = 0, ...options}: typeOptions = {},
+): Promise<void> | void {
+  prepareDocument(element.ownerDocument)
+
+  // we do not want to wrap in the asyncWrapper if we're not
+  // going to actually be doing anything async, so we only wrap
+  // if the delay is greater than 0
+
+  if (delay > 0) {
+    return getDOMTestingLibraryConfig().asyncWrapper(() =>
+      typeImplementation(this, element, text, {delay, ...options}),
+    )
+  } else {
+    return void typeImplementation(this, element, text, {delay, ...options})
+      // prevents users from dealing with UnhandledPromiseRejectionWarning
+      .catch(console.error)
+  }
+}
+
+async function typeImplementation(
+  userEvent: UserEvent,
   element: Element,
   text: string,
   {
@@ -31,7 +70,7 @@ export async function typeImplementation(
   // we use this workaround for now to prevent changing behavior
   if ((element as {disabled?: boolean}).disabled) return
 
-  if (!skipClick) this.click(element)
+  if (!skipClick) userEvent.click(element)
 
   // The focused element could change between each event, so get the currently active element each time
   const currentElement = () => getActiveElement(element.ownerDocument)

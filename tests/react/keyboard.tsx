55e194aa96c31844950fa5c74c5ff14a513f0f24
Philipp Fritsche <ph.fritsche@gmail.com>
2021-10-30 22:16:34 +0200

    fix(keyboard): set `KeyboardEvent.charCode` on `keypress` (#771)

--- tests/react/keyboard.tsx
+++ tests/react/keyboard.tsx
@@ -20,3 +20,17 @@ test.each([0, 1])('maintain cursor position on controlled input', () => {
   expect(screen.getByRole('textbox')).toHaveProperty('selectionStart', 2)
   expect(screen.getByRole('textbox')).toHaveProperty('selectionEnd', 2)
 })
+
+test('trigger Synthetic `keypress` event for printable characters', () => {
+  const onKeyPress = jest.fn<unknown, [React.KeyboardEvent]>()
+  render(<input onKeyPress={onKeyPress} />)
+  screen.getByRole('textbox').focus()
+
+  userEvent.keyboard('a')
+  expect(onKeyPress).toHaveBeenCalledTimes(1)
+  expect(onKeyPress.mock.calls[0][0]).toHaveProperty('charCode', 97)
+
+  userEvent.keyboard('[Enter]')
+  expect(onKeyPress).toHaveBeenCalledTimes(2)
+  expect(onKeyPress.mock.calls[1][0]).toHaveProperty('charCode', 13)
+})

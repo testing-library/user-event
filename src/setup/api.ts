3373b2f32f233a02262c3db0234316d2048992bd
Philipp Fritsche <ph.fritsche@gmail.com>
2021-11-24 12:21:58 +0000

    remove unused code

--- src/setup/api.ts
+++ src/setup/api.ts
@@ -22,23 +22,3 @@ export const userEventApi = {
   unhover,
   upload,
 } as const
-
-/** Which is the options parameter? */
-export const userEventApiOptionsParameter = {
-  clear: undefined,
-  click: 1,
-  copy: 0,
-  cut: 0,
-  dblClick: 1,
-  deselectOptions: 2,
-  hover: 1,
-  keyboard: 1,
-  paste: 1,
-  pointer: 1,
-  selectOptions: 2,
-  tab: 0,
-  tripleClick: 1,
-  type: 2,
-  unhover: 1,
-  upload: 3,
-} as const

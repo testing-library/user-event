df75e5f5a91fd3f00f7b3e4ac4a5532838eadb68
Philipp Fritsche <ph.fritsche@gmail.com>
2021-11-25 14:53:05 +0100

    fix(keyboard): parse keyboard input without nesting (#793)

---
+++ src/keyboard/parseKeyDef.ts
@@ -0,0 +1,51 @@
+import {readNextDescriptor} from '../utils'
+import {keyboardKey} from './types'
+
+/**
+ * Parse key defintions per `keyboardMap`
+ *
+ * Keys can be referenced by `{key}` or `{special}` as well as physical locations per `[code]`.
+ * Everything else will be interpreted as a typed character - e.g. `a`.
+ * Brackets `{` and `[` can be escaped by doubling - e.g. `foo[[bar` translates to `foo[bar`.
+ * Keeping the key pressed can be written as `{key>}`.
+ * When keeping the key pressed you can choose how long (how many keydown and keypress) the key is pressed `{key>3}`.
+ * You can then release the key per `{key>3/}` or keep it pressed and continue with the next key.
+ */
+export function parseKeyDef(keyboardMap: keyboardKey[], text: string) {
+  const defs: Array<{
+    keyDef: keyboardKey
+    releasePrevious: boolean
+    releaseSelf: boolean
+    repeat: number
+  }> = []
+
+  do {
+    const {
+      type,
+      descriptor,
+      consumedLength,
+      releasePrevious,
+      releaseSelf = true,
+      repeat,
+    } = readNextDescriptor(text)
+
+    const keyDef = keyboardMap.find(def => {
+      if (type === '[') {
+        return def.code?.toLowerCase() === descriptor.toLowerCase()
+      } else if (type === '{') {
+        return def.key?.toLowerCase() === descriptor.toLowerCase()
+      }
+      return def.key === descriptor
+    }) ?? {
+      key: 'Unknown',
+      code: 'Unknown',
+      [type === '[' ? 'code' : 'key']: descriptor,
+    }
+
+    defs.push({keyDef, releasePrevious, releaseSelf, repeat})
+
+    text = text.slice(consumedLength)
+  } while (text)
+
+  return defs
+}

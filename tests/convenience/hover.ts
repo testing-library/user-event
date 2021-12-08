00b745fd99da507ba209d7c20d50a1d4e644fc6f
Philipp Fritsche <ph.fritsche@gmail.com>
2021-12-08 14:18:29 +0000

    refactor(test): refactor hover tests

---
+++ tests/convenience/hover.ts
@@ -0,0 +1,39 @@
+import userEvent from '#src'
+import {setup} from '#testHelpers/utils'
+
+describe.each([
+  ['hover', {events: ['over', 'enter', 'move']}],
+  ['unhover', {events: ['move', 'leave', 'out']}],
+] as const)('%s', (method, {events}) => {
+  test(`${method} element`, async () => {
+    const {element, getEvents} = setup('<div></div>')
+
+    await userEvent[method](element)
+
+    for (const type of events) {
+      expect(getEvents(`pointer${type}`)).toHaveLength(1)
+      expect(getEvents(`mouse${type}`)).toHaveLength(1)
+    }
+
+    expect(getEvents('pointermove')).toHaveLength(1)
+    expect(getEvents('mousemove')).toHaveLength(1)
+  })
+
+  test('throw on pointer-events set to none', async () => {
+    const {element} = setup(`<div style="pointer-events: none"></div>`)
+
+    await expect(userEvent[method](element)).rejects.toThrowError(
+      /has or inherits pointer-events/i,
+    )
+  })
+
+  test('skip check for pointer-events', async () => {
+    const {element, getEvents} = setup(
+      `<div style="pointer-events: none"></div>`,
+    )
+
+    await userEvent[method](element, {skipPointerEventsCheck: true})
+
+    expect(getEvents('mousemove')).toHaveLength(1)
+  })
+})

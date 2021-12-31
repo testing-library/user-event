6ec470d342f87e984ab9dbb15802b8296e84d8e3
Philipp Fritsche <ph.fritsche@gmail.com>
2021-12-31 17:20:31 +0000

    fix: export types and commonjs bundle

--- build.js
+++ build.js
@@ -1,10 +1,9 @@
 ;(async () => {
   const child = require('child_process')
-  const fs = require('fs')
   const {build} = require('esbuild')
 
   await build({
-    outdir: 'dist',
+    outfile: 'dist/index.mjs',
     format: 'esm',
     target: 'es6',
     bundle: true,
@@ -12,12 +11,14 @@
     entryPoints: ['src/index.ts'],
   })
 
-  fs.writeFileSync(
-    'dist/package.json',
-    JSON.stringify({
-      type: 'module',
-    }),
-  )
+  await build({
+    outfile: 'dist/index.cjs',
+    format: 'cjs',
+    target: 'node12',
+    bundle: true,
+    external: ['@testing-library/dom'],
+    entryPoints: ['src/index.ts'],
+  })
 
   child.execSync('yarn tsc -p tsconfig.build.json')
 })()

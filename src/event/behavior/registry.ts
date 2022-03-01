4da47b1986021e13d03a3189c7cf3cfe24a17084
Philipp Fritsche <ph.fritsche@gmail.com>
2022-03-01 11:42:17 +0000

    clean up

--- src/event/behavior/registry.ts
+++ src/event/behavior/registry.ts
@@ -7,7 +7,7 @@ export interface BehaviorPlugin<Type extends EventType> {
     target: Element,
     config: Config,
   ): // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  void | (() => void) | (() => Promise<void>)
+  void | (() => void)
 }
 
 export const behavior: {

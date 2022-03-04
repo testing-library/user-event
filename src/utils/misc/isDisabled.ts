1a00fdfc3085addc4792897aecda202e07c3f5d2
Philipp Fritsche <ph.fritsche@gmail.com>
2022-03-04 11:58:26 +0100

    fix: check for inherited `:disabled` (#872)

--- src/utils/misc/isDisabled.ts
+++ src/utils/misc/isDisabled.ts
@@ -1,5 +1,37 @@
-// This should probably be extended with checking the element type
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled
+import {isElementType} from './isElementType'
+
+// This should probably just rely on the :disabled pseudo-class, but JSDOM doesn't implement it properly.
 export function isDisabled(element: Element | null): boolean {
-  return Boolean(element && (element as {disabled?: boolean}).disabled)
+  for (let el = element; el; el = el.parentElement) {
+    if (
+      isElementType(el, [
+        'button',
+        'input',
+        'select',
+        'textarea',
+        'optgroup',
+        'option',
+      ])
+    ) {
+      if (el.hasAttribute('disabled')) {
+        return true
+      }
+    } else if (isElementType(el, 'fieldset')) {
+      if (
+        el.hasAttribute('disabled') &&
+        !el.querySelector(':scope > legend')?.contains(element)
+      ) {
+        return true
+      }
+    } else if (el.tagName.includes('-')) {
+      if (
+        (el.constructor as {formAssociated?: boolean}).formAssociated &&
+        el.hasAttribute('disabled')
+      ) {
+        return true
+      }
+    }
+  }
+
+  return false
 }

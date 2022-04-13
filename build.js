acbe109c52d5075dc36ac69a3153e991f3433625
Chris Plummer <chris.plummer@doma.com>
2022-04-13 16:03:41 -0500

    write a package.json file to the esm/ directory to tell Node that it should use ES modules

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
 ;(async () => {
   const child = require('child_process')
-  const { build } = require('esbuild')
+  const {build} = require('esbuild')
+  const fs = require('fs')
 
+  // put the ESM output in a separate directory to avoid using the .mjs extension
   await build({
     outfile: 'dist/esm/index.js',
     format: 'esm',
@@ -10,6 +12,10 @@
     external: ['@testing-library/dom'],
     entryPoints: ['src/index.ts'],
   })
+
+  // Node will interpret the JS output as CommonJS without a specific type declaration
+  // see https://nodejs.org/dist/latest/docs/api/packages.html#packagejson-and-file-extensions
+  fs.writeFileSync('dist/esm/package.json', '{ "type": "module" }')
 
   await build({
     outfile: 'dist/index.js',

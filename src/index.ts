beefc2672804a5bf13419d1299f47569b098bb2e
Vitaly Baev <ping@baev.dev>
2022-07-13 17:53:23 +0300

    chore: expose `UserEvent` type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {userEvent as default} from './setup'
+export {userEvent as default, UserEvent} from './setup'
 export type {keyboardKey} from './keyboard'
 export type {pointerKey} from './pointer'
 export {PointerEventsCheckLevel} from './options'

d82cbef924099347028d49dcf83da9b86da0cef2
Violet Peña <vpena@blend.com>
2022-08-31 16:09:36 -0700

    Simplify.

--- src/event/radio.ts
+++ src/event/radio.ts
@@ -2,11 +2,6 @@ import {type Instance} from '../setup'
 import {getWindow, isDisabled} from '../utils'
 import {focusElement} from './focus'
 
-function setNewActiveRadio(instance: Instance, el: HTMLInputElement & {type: 'radio'}) {
-  focusElement(el)
-  instance.dispatchUIEvent(el, 'click')
-}
-
 export function walkRadio(
   instance: Instance,
   el: HTMLInputElement & {type: 'radio'},
@@ -24,22 +19,8 @@ export function walkRadio(
   if (group.length <= 1) {
     return;
   }
-  const nextRadioIndex = group.indexOf(el) + direction;
-  // Multiple radio buttons
-  if (direction === 1) {
-    // Moving forwards in group
-    if (nextRadioIndex < group.length) {
-      // Don't need to wrap around
-      return setNewActiveRadio(instance, group[nextRadioIndex])
-    }
-    // Special case: do need to wrap around
-    return setNewActiveRadio(instance, group[0])
-  }
-  // Moving backwards in group
-  if (nextRadioIndex >= 0) {
-    // Don't need to wrap around
-    return setNewActiveRadio(instance, group[nextRadioIndex])
-  }
-  // Special case: do need to wrap around
-  setNewActiveRadio(instance, group[group.length - 1])
+  // Multiple buttons found
+  const nextRadioElement = group[(group.indexOf(el) + direction + group.length) % group.length];
+  focusElement(nextRadioElement)
+  instance.dispatchUIEvent(nextRadioElement, 'click')
 }

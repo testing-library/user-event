435ff118e2ff0a577cd898f7df4be7e1342ad729
Jack Russell <jackrussell1996@gmail.com>
2022-10-10 22:35:07 -0700

    Convert "export {X as default}" to "export default"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export {userEvent as default} from './setup'
+import {userEvent} from './setup'
+export default userEvent
 export type {keyboardKey} from './system/keyboard'
 export type {pointerKey} from './system/pointer'
 export {PointerEventsCheckLevel} from './options'

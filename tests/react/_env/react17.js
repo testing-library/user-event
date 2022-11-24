0d2810ea91efad9d8835bb37fc53d79965e1f652
Philipp Fritsche <ph.fritsche@gmail.com>
2022-11-24 11:46:46 +0100

    update jest-environment import

--- tests/react/_env/react17.js
+++ tests/react/_env/react17.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const jsdomEnv = require('jest-environment-jsdom')
+const {TestEnvironment} = require('jest-environment-jsdom')
 
-module.exports = class React17 extends jsdomEnv {
+module.exports = class React17 extends TestEnvironment {
   async setup() {
     await super.setup()
     this.global.REACT_VERSION = 17

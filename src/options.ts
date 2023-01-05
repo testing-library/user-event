7b91c2f4a8c5b03b69fce2be0616e87558e07dd3
Kevin Bon <kbon@hubspot.com>
2023-01-05 11:33:41 -0500

    wording consistency

--- src/options.ts
+++ src/options.ts
@@ -36,10 +36,10 @@ export interface Options {
   autoModify?: boolean
 
   /**
-   * Automatically focus the active document before interacting with the keyboard
+   * Automatically focus the active document before interacting with the keyboard.
    *
-   * Recommended to enable whenever the test is running against a real web-browser
-   * and a real user interaction may interfere the test.
+   * Recommended to enable it whenever the test is running against a real web browser and
+   * a real user interaction may interfere with the test.
    *
    * @default false
    */

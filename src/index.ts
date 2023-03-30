20334d7e6a43a23ca0b872226fc2ef40d438f824
Joshua Jacobowitz <joshua.jacobowitz@yahoo.de>
2023-03-30 13:59:28 +0200

    feat: export UserEvent type (#1085)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {userEvent as default} from './setup'
+export type {UserEvent} from './setup/setup'
 export type {keyboardKey} from './system/keyboard'
 export type {pointerKey} from './system/pointer'
 export {PointerEventsCheckLevel, type Options} from './options'

a809bd331449cc369ab6795374ed76cff5cb84d6
Joshua Jacobowitz <joshua.jacobowitz@yahoo.de>
2023-03-30 13:58:59 +0200

    feat: export Options type (#1111)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {userEvent as default} from './setup'
 export type {keyboardKey} from './system/keyboard'
 export type {pointerKey} from './system/pointer'
-export {PointerEventsCheckLevel} from './options'
+export {PointerEventsCheckLevel, type Options} from './options'

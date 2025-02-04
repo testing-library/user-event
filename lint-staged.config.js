3d7005a70b59c4a0de00eed514678bd2e61e4908
Philipp Fritsche <ph.fritsche@gmail.com>
2025-02-04 16:52:24 +0100

    chore: update workflow files (#1271)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.+(json|yml|yaml|css|less|scss|md|gql|graphql|mdx|vue)': [
+  '*.+(json|css|less|scss|md|gql|graphql|mdx|vue)': [
     `kcd-scripts format`,
   ],
   '*.+(js|jsx|mjs|cjs|ts|tsx|mts|cts)': [
